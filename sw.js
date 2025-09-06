d20d805e9a105dc6753d6c7e93d4feb05c952294
Anton Kolinko <kolinko.ant@gmail.com>
2025-09-06 10:08:23 +0200

    redesign

--- sw.js
+++ sw.js
@@ -1,9 +1,15 @@
-const CACHE = 'mealreel-v1';
-const ASSETS = ['/', '/index.html', '/manifest.webmanifest'];
+const CACHE = 'mealreel-client-v1';
+const BASE = new URL(self.registration.scope).pathname.replace(/\/$/, '');
+const ASSETS = [ `${BASE}/`, `${BASE}/index.html`, `${BASE}/manifest.webmanifest` ];
 
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
+
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+});
+
 self.addEventListener('fetch', e=>{
   const url = new URL(e.request.url);
   if (url.origin === location.origin || url.hostname === 'img.youtube.com') {
